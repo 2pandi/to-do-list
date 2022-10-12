081c828a13f7fe591be56b644af3e36fb4393f8c
2pandi <coder.2pandi@gmail.com>
2022-10-12 21:47:11 +0900

    :lock: chore: Set fbase for Auth

--- src/fbase.js
+++ src/fbase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBb8JHYgBFmjlKGSLWHJvW88XvhsZgxgbc",
-  authDomain: "todolist-f3b03.firebaseapp.com",
-  projectId: "todolist-f3b03",
-  storageBucket: "todolist-f3b03.appspot.com",
-  messagingSenderId: "711994400424",
-  appId: "1:711994400424:web:8489c79064fc9b8270ec9d",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
